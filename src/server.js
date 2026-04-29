fc66d8523bb2aa2d930bf236a05603ad1eb32029
bobgus39 <bobgus39@gmail.com>
2026-04-29 21:00:37 +0200

    cors upload

--- src/server.js
+++ src/server.js
@@ -7,7 +7,9 @@ const ordersRoutes = require("./routes/orders");
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: "*"
+}));
 app.use(express.json());
 
 app.use("/api/orders", ordersRoutes);
